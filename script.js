0777f2a0b61e8b4e5bc168bdc41f041b046d9866
zeetaxsolution <advosahiba183@gmail.com>
2026-02-28 08:14:48 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-// Example JS - simple form alert
-const form = document.querySelector('form');
-form.addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('Thank you! We will contact you soon.');
-    form.reset();
-});
+
